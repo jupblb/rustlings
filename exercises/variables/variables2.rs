a8935b9ecf3b2f22e6aab42de2b7ea3599419447
jupblb <mpkielbowicz@gmail.com>
2020-03-13 19:46:59 +0100

    Finished variables exercises

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,10 +1,13 @@
 // variables2.rs
 // Make me compile! Execute the command `rustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
+use std::io;
 
 fn main() {
-    let x;
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let x: i32 = input.trim().parse().unwrap();
+
     if x == 10 {
         println!("Ten!");
     } else {
